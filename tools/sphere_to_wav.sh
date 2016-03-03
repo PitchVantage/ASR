688cea82f2f7bd59483ea9b7a49d8cb5c4757e2f
michaelcapizzi <michael.capizzi@pitchvantage.com>
2016-03-02 20:48:37 -0700

    ASR-8 #comment updated script with location of sph2pipe in kaldi/tools/ #done

--- tools/sphere_to_wav.sh
+++ tools/sphere_to_wav.sh
@@ -2,14 +2,14 @@
 
 
 #take as input:
-# (0) the location of the entire WSJ collection
-# (1) the location of sph2pipe
+# $1 the location of the entire WSJ collection
+
 
 #capture all files recursively with type .WV1
 ALLFILES=( $(find $1 -name "*.WV1" -type f) )
 
 #iterate over list of files and run sph2pipe
 for i in ${ALLFILES[@]}; do
-    $2/sph2pipe $i "${i%.WV1}.wav"              #renames from .WV1 to .wav
+    /media/mcapizzi/data/Github/kaldi/tools/sph2pipe_v2.5/sph2pipe $i "${i%.WV1}.wav"              #renames from .WV1 to .wav
 done
 
