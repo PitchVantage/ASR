e30b05aa3d9f38df64838e6c4a12c94f708a057b
michaelcapizzi <michael.capizzi@pitchvantage.com>
2016-03-02 21:25:55 -0700

    ASR-8 #comment updated script with location of sph2pipe in kaldi/tools/ #done

--- tools/sphere_to_wav.sh
+++ tools/sphere_to_wav.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
+# $1 = location of the entire WSJ collection
+# $2 = location for output files  NOTE: if no second argument given,
+# output is in same location as original file
 
-#take as input:
-# $1 the location of the entire WSJ collection
-
-
-#capture all files recursively with type .WV1
+#capture all files in $1 recursively with type .WV1
 ALLFILES=( $(find $1 -name "*.WV1" -type f) )
 
 #iterate over list of files and run sph2pipe
 for i in ${ALLFILES[@]}; do
-    /media/mcapizzi/data/Github/kaldi/tools/sph2pipe_v2.5/sph2pipe $i "${i%.WV1}.wav"              #renames from .WV1 to .wav
+    #if second argument (location) is given, output to that location
+    if [ "$#" -gt 1 ]; then
+        base=$(basename $i)
+        /media/mcapizzi/data/Github/kaldi/tools/sph2pipe_v2.5/sph2pipe $i $2"${base%.WV1}.wav"       #renames from .WV1 to .wav
+    #else save to same location as original
+    else
+        /media/mcapizzi/data/Github/kaldi/tools/sph2pipe_v2.5/sph2pipe $i "${i%.WV1}.wav"       #renames from .WV1 to .wav
+    fi
 done
 
