f5989d86cc426baa4fdb38ba197aa47b7ff00d07
michaelcapizzi <michael.capizzi@pitchvantage.com>
2016-03-02 20:40:35 -0700

    ASR-8 #comment added shell script to convert sphere to wav #done

--- tools/sphere_to_wav.sh
+++ tools/sphere_to_wav.sh
@@ -4,10 +4,12 @@
 #take as input:
 # (0) the location of the entire WSJ collection
 # (1) the location of sph2pipe
-# ./sph2pipe [input] [output.wav]
 
-for disc in $( ls ); do         #13_1_1
-    #TODO capture this to a list
-    find $i -type f             #will print out list of all files recursively
-    #TODO filter list for .WV1
-    #run ./sphpipe
+#capture all files recursively with type .WV1
+ALLFILES=( $(find $1 -name "*.WV1" -type f) )
+
+#iterate over list of files and run sph2pipe
+for i in ${ALLFILES[@]}; do
+    $2/sph2pipe $i "${i%.WV1}.wav"              #renames from .WV1 to .wav
+done
+
