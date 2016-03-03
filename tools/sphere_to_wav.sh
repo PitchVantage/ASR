6f61c245b6cd2bef64661d3109ed57e9ab15b7a9
michaelcapizzi <michael.capizzi@pitchvantage.com>
2016-03-02 21:39:39 -0700

    ASR-8 #comment updated script with location of sph2pipe in kaldi/tools/  #done

--- tools/sphere_to_wav.sh
+++ tools/sphere_to_wav.sh
@@ -14,13 +14,13 @@ for i in ${ALLFILES[@]}; do
         base=$(basename $i)
         #get local path to /tools/sph2pipe_v2.5
         localPath=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )/sph2pipe_v2.5
-#        /media/mcapizzi/data/Github/kaldi/tools/sph2pipe_v2.5/sph2pipe $i $2"${base%.WV1}.wav"       #renames from .WV1 to .wav
+        #run sph2pipe from local kaldi version found in tools
         $localPath/sph2pipe $i $2"${base%.WV1}.wav"       #renames from .WV1 to .wav
     #else save to same location as original
     else
         #get local path to /tools/sph2pipe_v2.5
         localPath=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )/sph2pipe_v2.5
-        #run sph2pipe from local kaldi
+        #run sph2pipe from local kaldi version found in tools
         $localPath/sph2pipe $i "${i%.WV1}.wav"       #renames from .WV1 to .wav
     fi
 done
