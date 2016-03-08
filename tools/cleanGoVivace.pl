6ed75c1618215cc41820fb1c9df320f773661621
michaelcapizzi <michael.capizzi@pitchvantage.com>
2016-03-08 09:32:22 -0700

    built method to clean transcripts

--- tools/cleanGoVivace.pl
+++ tools/cleanGoVivace.pl
@@ -2,16 +2,33 @@
 use warnings;
 use strict;
 
+# cleans transcripts before computing WER by
+# removing periods and uppercase
+# puts new file "-cleaned.txt" in same location as original
+
 # ARGV[0] = transcript to be cleaned
 
-#open file
 
+#open file
 my $transcript_file = $ARGV[0];
 
-open TRANSCRIPT, $transcript_file or die "Can't open file. Are you sure you have the correct path?";
+open(TRANSCRIPT, $transcript_file) or die "Can't open file. Are you sure you have the correct path?";
+
+use File::Basename;
+
+#get just filename (without extension)
+my ($filename, $dirs, $suffix) = fileparse($transcript_file);
+$filename =~ s/(.+)\..+/$1/;        #removes file extension
+#create new file
+my $newFilePath = "$dirs$filename-cleaned.txt";
+open(my $output, ">", $newFilePath);
 
 while (my $line = <TRANSCRIPT>) {
-    chomp my $line;
-    my $lowerCase =~ s/([A-Z])/lc($1)/g;
-    print $lowerCase;
+    $line =~ s/([A-Z])/\L$1/g;      #convert all to lowercase
+    $line =~ s/\.//g;               #remove periods
+    $line =~ s/  / /g;              #remove extra spaces
+    print($output $line);
 }
+
+close TRANSCRIPT;
+close $output;
