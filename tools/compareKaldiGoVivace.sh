2bec9e96438a1e6f7cba467f08c9bb41af16f450
michaelcapizzi <michael.capizzi@pitchvantage.com>
2016-03-08 12:05:51 -0700

    building shell script to run comparisons results

--- tools/compareKaldiGoVivace.sh
+++ tools/compareKaldiGoVivace.sh
@@ -2,37 +2,41 @@
 
 #compares WER results between a Kaldi system and GoVivace
 #must be run from tools/
+
 #TODO parallelize: http://stackoverflow.com/questions/38160/parallelize-bash-script-with-maximum-number-of-processes
 
-# $1 = location of wav files to test
-# $2 = location to save results
-# $3 = location of cleaned (using prepareTranscript.pl) gold transcripts
+# $1 = location to save results
+# $2 = location of cleaned (using prepareTranscript.pl) gold transcripts
             #[same name as wav].gold
-# $4 = location of cleaned (using prepareTranscript.pl??) kaldi transcripts
+# $3 = location of cleaned (using prepareTranscript.pl??) kaldi transcripts
             #[same name as wav].kaldi
             #TODO check that prepareTranscript.pl is needed
-# $5 = location of cleaned (using prepareTranscript.pl) GoVivace transcripts
+# $4 = location of cleaned (using prepareTranscript.pl) GoVivace transcripts
             #[same name as wav].goV
+# $5 = optional: location of wav files to test
 
 
-waves_dir=$1
-results=$2
-gold_dir=$3
-kaldiTranscripts_dir=$4
-goVivaceTranscripts_dir=$5
-#assignment of this variable depends on whether $5 is empty or not
-if [ "$(ls -A $5)" ]; then
+
+results=$1
+gold_dir=$2
+kaldiTranscripts_dir=$3
+goVivaceTranscripts_dir=$4
+#assignment of this variable depends on whether $4 is empty or not
+if [ "$(ls -A $4)" ]; then
 	requireTranscribe=false
 else
 	requireTranscribe=true
+fi
+#assignment of this variable depends on whether $5 exists or not
+if [ -z $5 ]; then
+    :               #do nothing
+else
+    waves_dir=$5    #assign variable
 fi
 
 echo "Cataloging files"
 
 #TODO is this cataloging necessary?
-#print all filenames in $waves_dir to file
-ls -1 $waves_dir > waves_all.list
-
 #print all filenames in $gold_dir to file
 ls -1 $gold_dir > goldTranscripts_all.list
 
@@ -44,6 +48,7 @@ ls -1 $kaldiTranscripts_dir > predictKaldiTranscripts_all.list
 #if goVivace transcription has already occurred
 # print all filenames to file
 if [ $requireTranscribe == false ]; then
+    #print all filenames in $goVivaceTrnascripts_dir to file
     ls -1 $goVivaceTranscripts_dir > predictGoVivaceTranscripts_all.list
 
     #sort files by bytes (kaldi-style) and resave
@@ -53,14 +58,14 @@ if [ $requireTranscribe == false ]; then
 
     #TODO ensure all files that are needed exist
 
-    #TODO iterate through all files in list and run .compute-wer
+    #iterate through all files in list and run .compute-wer
     for fileName in goldTranscripts_all.list; do
         #get basename
         base=$(basename $fileName)
         #build filenames
-        gold=$base.gold
-        kaldi=$base.kaldi
-        goV=$base.goV
+        gold=$gold_dir$base.gold
+        kaldi=$kaldiTranscripts_dir$base.kaldi
+        goV=$goVivaceTranscripts_dir$base.goV
         #prepare results file
         echo $base >> ${results}
         echo "=======" >> ${results}
@@ -74,12 +79,32 @@ if [ $requireTranscribe == false ]; then
         ../src/bin/compute-wer --text --mode=present ark:${gold} ark:${goV} >> ${results}
     done
 
-
 #if goVivace transcriptions haven't already been created
 else
-    #TODO callGoVivace
-        #[same name as wav].goV
-    #TODO call compute-wer
+    #print all filenames in $waves_dir to file
+    ls -1 $waves_dir > waves_all.list
+
+    #sort files by bytes (kaldi-style) and resave
+    for fileName in waves_all.list; do
+        LC_ALL=C sort -i $fileName -o $fileName;
+    done
+
+    #iterate through wave files
+    for fileName in waves_all.list; do
+        #get basename
+        base=$(basename $fileName)
+        #build filenames
+        raw=$goVivaceTranscripts_dir$base.raw
+        goV=$goVivaceTranscripts_dir$base.goV
+        #call goVivace client
+            #name file .raw
+        ./callGoVivace.sh text $fileName $raw
+        #clean raw transcript
+            #[same name as wav].goV
+        ./prepareTranscript.pl $raw $goV
+        #prepare results file
+
+        #TODO call compute-wer
 fi
 
 
