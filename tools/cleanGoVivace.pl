8ee3ba3abcab9c094f356d72dd9ad4719fdb76d1
michaelcapizzi <michael.capizzi@pitchvantage.com>
2016-03-08 09:37:55 -0700

    updated to take optional second argument

--- tools/cleanGoVivace.pl
+++ tools/cleanGoVivace.pl
@@ -7,6 +7,7 @@ use strict;
 # puts new file "-cleaned.txt" in same location as original
 
 # ARGV[0] = transcript to be cleaned
+# ARGV[1] = optional: full path location for cleaned file
 
 
 #open file
@@ -19,10 +20,18 @@ use File::Basename;
 #get just filename (without extension)
 my ($filename, $dirs, $suffix) = fileparse($transcript_file);
 $filename =~ s/(.+)\..+/$1/;        #removes file extension
+
+my $newFilePath;
 #create new file
-my $newFilePath = "$dirs$filename-cleaned.txt";
+if (scalar @ARGV == 1) {            #if no second command line argument given
+    $newFilePath = "$dirs$filename-cleaned.txt";
+} else {
+    $newFilePath = $ARGV[1];        #if path given as second command line argument
+}
+
 open(my $output, ">", $newFilePath);
 
+#clean
 while (my $line = <TRANSCRIPT>) {
     $line =~ s/([A-Z])/\L$1/g;      #convert all to lowercase
     $line =~ s/\.//g;               #remove periods
