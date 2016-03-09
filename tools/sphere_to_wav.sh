34e097a18c28746a548aacda99cad1ea0148f1da
michaelcapizzi <michael.capizzi@pitchvantage.com>
2016-03-09 14:00:18 -0700

    updated sphere_to_wav with corrections

--- tools/sphere_to_wav.sh
+++ tools/sphere_to_wav.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # $1 = location of the entire collection
-# $2 = file extension of sphere (eg. *.WV1)
+# $2 = file extension of sphere (eg. .WV1)
 # $3 = location for output files  NOTE: if no second argument given
 
 # output is in same location as original file
 
 #capture all files in $1 recursively with type $2
-ALLFILES=( $(find $1 -name $2 -type f) )
+ALLFILES=( $(find $1 -name *$2 -type f) )
 
 #iterate over list of files and run sph2pipe
 for i in ${ALLFILES[@]}; do
@@ -17,15 +17,15 @@ for i in ${ALLFILES[@]}; do
         #get local path to /tools/sph2pipe_v2.5
         localPath=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )/sph2pipe_v2.5
         #run sph2pipe from local kaldi version found in tools
-#        $localPath/sph2pipe $i $2"${base%.WV1}.wav"       #renames from original file type to .wav
+#        $localPath/sph2pipe $i -f wav $3"${base%.WV1}.wav"       #renames from original file type to .wav
         $localPath/sph2pipe $i $3"${base%$2}.wav"       #renames from original file type to .wav
     #else save to same location as original
     else
         #get local path to /tools/sph2pipe_v2.5
         localPath=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )/sph2pipe_v2.5
         #run sph2pipe from local kaldi version found in tools
-#        $localPath/sph2pipe $i "${i%.WV1}.wav"       #renames from original file type to .wav
-        $localPath/sph2pipe $i "${i%$2}.wav"       #renames from original file type to .wav
+#        $localPath/sph2pipe $i -f wav "${i%.WV1}.wav"       #renames from original file type to .wav
+        $localPath/sph2pipe $i -f wav "${i%$2}.wav"       #renames from original file type to .wav
     fi
 done
 
