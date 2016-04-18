2376cdc399ee4fce46802d2af1c42c0696b94014
HLT-ADMIN <mkittles@email.arizona.edu>
2016-04-18 13:23:55 -0700

    Adding lm script tool to replace unkown words with XYZ. Removing old file from github. Updating TOOLS_INDEX.MD.

---
+++ tools/PVlm_pre/Replace_w_XYZ.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+#/usr/local/bin
+#!/usr/bin/python
+
+import re, locale
+import sys
+
+#Script: Replace_w_XYZ.py
+#Author: Megan Willi
+#Last Updated: 04_18_16
+
+#Purpose: Reads in a clean, upper case, formatted lm transcript and replaces any words not in the lexicon with XYZ. Outputs a lm transcript file where any word not in the lexicon is replaced by XYZ.
+
+#Command Line: ./Replace_w_XYZ.py [path/to/lexicon/file] [path/to/CLEAN_transcripts/file] [path/to/output/transcripts/file]
+
+#Example Command Line: ./Replace_w_XYZ.py lexicon.txt CLEAN_transcript.txt output_transcript.txt
+
+#Command Line Variables:
+#sys.argv[1]= lexicon.txt
+#sys.argv[2]= CLEAN_transcript.txt
+#sys.argv[3]= output_transcript.txt
+
+#Reads in lexicon .txt file.
+f = open(sys.argv[1], "rb")
+
+#Creates a list of words in the lexicon.
+Words=[]
+for line in f:
+   line_split = line.split("\t")
+   id = line_split[0]
+   Words.append(id)
+f.close
+
+#For each word in the transcript, if it is NOT in the lexicon, then replace with XYZ. Write to an output.txt file.
+f = open(sys.argv[2], "rb")
+f3= open(sys.argv[3], "wb")
+for line in f:
+    NEW_line=[]
+    line_split = line.rstrip().split(" ")
+    for token in line_split:
+        if token not in Words:
+            NEW_line.append("XYZ")
+        else:
+            NEW_line.append(token)
+    Sentence=" ".join(NEW_line)
+    f3.write(Sentence+"\n")
+f.close
+f3.close
+
+
+
