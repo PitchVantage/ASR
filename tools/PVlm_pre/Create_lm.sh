9236eb7fcd94806b27ea874dd8fb1e6ab0c7f128
HLT-ADMIN <mkittles@email.arizona.edu>
2016-04-18 14:47:54 -0700

    Adding tools script to create lm from cleaned lm transcript. Remove old script. Update TOOLS_INDEX.MD.

---
+++ tools/PVlm_pre/Create_lm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Script: Create_lm.sh
+#Author: Megan Willi
+#Last Updated: 04_18_16
+
+#Purpose:Read in clean, formatted, uppercase lm transcript. Output language model created from the lm transcript.
+#Command Line: ./Create_lm.sh [path/to/input/CLEAN_transcripts/file] [path/to/output/lm.arpabo/file]
+
+#Example Command Line: ./Create_lm.sh CLEAN_transcript.txt Output_lm.arpabo
+
+#Command Line Variables:
+#$1= CLEAN_transcript.txt
+#$2= Output_lm.arpabo
+
+input=$1
+output=$2
+
+export IRSTLM=/Volumes/poo/ASR/tools/extras/irstlm
+export PATH=${PATH}:${IRSTLM}/bin
+
+
+build-lm.sh -i $1 -n 3 -o train.ilm.gz -k 5
+
+compile-lm --text=yes train.ilm.gz $output
+
+rm train.ilm.gz
